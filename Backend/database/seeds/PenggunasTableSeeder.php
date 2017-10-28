3ed9c0990052c2b4e7665ead658f137d7b081869
Herdito Ibnu Dewangkoro <herditoibnu@gmail.com>
2017-10-28 22:02:10 +0700

    create pengguna seeder

---
+++ Backend/database/seeds/PenggunasTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Pengguna;
+
+class PenggunasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Pengguna::truncate();
+
+        $faker = \Faker\Factory::create();
+        
+        for ($i = 0; $i < 10; $i++) {
+            Pengguna::create([
+                'penggunas_dev_id' => $faker->uuid,
+                'penggunas_nama' => $faker->name,
+                'penggunas_alamat' => $faker->address,
+                'penggunas_email' => $faker->safeEmail,
+                'penggunas_no_hp' => $faker->phoneNumber,
+                'penggunas_lat' => $faker->latitude($min = -90, $max = 90) ,
+                'penggunas_long' => $faker->longitude($min = -180, $max = 180),
+            ]);
+        }
+    }
+}
