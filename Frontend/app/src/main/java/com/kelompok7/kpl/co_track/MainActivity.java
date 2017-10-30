afd1399679fb9f876ba9a306529a6fdb450f429c
jeffrynasri <jeffryfaruki@Gmail.com>
2017-10-30 09:02:14 +0700

    Penambahan intent agar map bisa tampil

--- Frontend/app/src/main/java/com/kelompok7/kpl/co_track/MainActivity.java
+++ Frontend/app/src/main/java/com/kelompok7/kpl/co_track/MainActivity.java
@@ -1,13 +1,28 @@
 package com.kelompok7.kpl.co_track;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnMapActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
+        btnMapActivity = (Button)findViewById(R.id.btn_map);
+        btnMapActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_map:
+                Intent moveIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(moveIntent);
+                break;
+        }
     }
 }
