6d04a445d88e3b307556198439741ac7c57e3550
Majid <aulmajid@gmail.com>
2017-10-31 09:51:52 +0700

    registeractivity

--- Frontend/app/src/main/java/com/kelompok7/kpl/co_track/activities/MainActivity.java
+++ Frontend/app/src/main/java/com/kelompok7/kpl/co_track/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.kelompok7.kpl.co_track.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,21 +11,33 @@ import com.kelompok7.kpl.co_track.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnMapActivity;
+    private final String TAG = getClass().getSimpleName();
+
+    private Button btnMaps;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnMapActivity = (Button) findViewById(R.id.btn_map);
-        btnMapActivity.setOnClickListener(this);
+
+        btnMaps = (Button) findViewById(R.id.btn_maps);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+
+        btnMaps.setOnClickListener(this);
+        btnRegister.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        Intent moveIntent;
         switch (view.getId()) {
-            case R.id.btn_map:
-                Intent moveIntent = new Intent(MainActivity.this, MapsActivity.class);
+            case R.id.btn_maps:
+                moveIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btn_register:
+                moveIntent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(moveIntent);
                 break;
         }
