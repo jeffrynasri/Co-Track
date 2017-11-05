4a454aa02a5c057704844ba754b83a359117be98
jeffrynasri <jeffryfaruki@Gmail.com>
2017-11-05 09:00:50 +0700

    Ganti menu navigation

--- app/src/main/java/com/kelompok7/kpl/co_track/activities/MainActivity.java
+++ app/src/main/java/com/kelompok7/kpl/co_track/activities/MainActivity.java
@@ -112,17 +112,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.all_user) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.subscribed_user) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.about) {
 
         }
 
